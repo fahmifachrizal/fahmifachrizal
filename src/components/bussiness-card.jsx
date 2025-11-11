429292a6eb6078ef7fda01b2f916acf6138eda59
Fahmi Fachrizal <m.fahmi.fachrizal@gmail.com>
2025-11-11 10:55:55 +0700

    init

---
+++ src/components/bussiness-card.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function BussinessCard() {
+  return (
+    <div>BussinessCard</div>
+  )
+}
+
+export default BussinessCard
---
+++ src/layout/preview.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function PreviewLayout() {
+  return (
+    <div>PreviewLayout</div>
+  )
+}
+
+export default PreviewLayout
