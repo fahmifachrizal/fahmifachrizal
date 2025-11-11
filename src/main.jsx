ae1d9fdb274b2e6040c48c2f0e3b52f50196f5d8
Fahmi Fachrizal <m.fahmi.fachrizal@gmail.com>
2025-11-11 11:20:13 +0700

    tansatack configured

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,19 @@
 import { StrictMode } from "react"
 import { createRoot } from "react-dom/client"
 import "./index.css"
-import App from "./App.jsx"
 import { ThemeProvider } from "./providers/theme-provider"
+import { RouterProvider, createRouter } from '@tanstack/react-router'
+
+// Import the generated route tree
+import { routeTree } from './routeTree.gen'
+
+// Create a new router instance
+const router = createRouter({ routeTree })
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-      <App />
+      <RouterProvider router={router} />
     </ThemeProvider>
   </StrictMode>
 )
